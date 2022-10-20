be2876c4c3a641c209e637119ba69d4f904cc048
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2022-10-20 09:24:13 +0200

    Lab2

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Services;
+
+namespace WebApplication3.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var calculateInstance = new Calculate(23);
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        public List<int> GetValue()
+        {
+            
+            var calculateInstance = new Calculate(23);
+            var calculateInstance2 = new Calculate(45);
+
+            return new List<int>() { calculateInstance2.GetValue(), calculateInstance.GetValue() };
+        }
+
+    }
+}
---
+++ Services/Calculate.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApplication3.Services
+{
+    public class Calculate
+    {
+        public Calculate(int initVal)
+        {
+            Value = initVal;
+        }
+
+        private int Value { get; set; }
+
+        public void Increase(int value)
+        {
+            this.Value += value;
+        }
+
+        public void Decrease(int value)
+        {
+            this.Value -= value;
+        }
+
+        public int GetValue()
+        {
+            return Value;
+        }
+    }
+}
