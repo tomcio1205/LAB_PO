76edfeb155022e10975cf920ec93cc6532d7b27a
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2022-10-27 09:26:14 +0200

    Lab3

---
+++ Controllers/LAB3/RectangleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Services.LAB3;
+
+
+namespace WebApplication3.Controllers.LAB3
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class RectangleController : ControllerBase
+    {
+        [HttpGet]
+        public int GetArea()
+        {
+            var rec = new Rectangle(23, 23);
+            return rec.GetArea(2);
+        }
+
+        [HttpGet]
+        public int GetSumArea()
+        {
+            var rec = new Rectangle(4, 4);
+            var rec2 = new Rectangle(3, 3);
+            var result = rec + rec2;
+            return result.GetArea();
+        }
+
+    }
+}
---
+++ Services/LAB3/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Common;
+
+namespace WebApplication3.Services.LAB3
+{
+    public class Rectangle: Shape
+    {
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public static Rectangle operator +(Rectangle a, Rectangle b)
+        {
+            return new Rectangle(a.Width + b.Width, a.Height + b.Height);
+        }
+    }
+}
