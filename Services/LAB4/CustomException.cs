3712d417be7b9d6fcb5a69cbcf54eaf113c9de8e
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2022-11-10 11:16:36 +0100

    LAB5

---
+++ Services/LAB4/CustomException.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication3.Services.LAB4
+{
+    public class InvalidDepartmentException : Exception
+    {
+        public InvalidDepartmentException() : base() { }
+        public InvalidDepartmentException(string message) : base(message) { }
+        public InvalidDepartmentException(string message, Exception inner) : base(message, inner) { }
+    }
+}
